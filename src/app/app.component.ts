6b6179067168e89b45b2392e81d2a59e24388512
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2022-03-21 02:14:21 -0500

    adding imports on app.module.ts files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ProductApiService } from './Infrastructure/driven-adapter/product-api/product-api-service/product-api-service.';
+import { Component, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  response$ ;
+  datos;
   title = 'invoicing';
+
+  constructor(private _productApiService : ProductApiService) {
+  }
+  ngOnInit(): void {
+
+   
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { ProductGateway } from 'src/app/Domain/Models/Product/Gateway/product-gateway';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductApiService } from './Infrastructure/driven-adapter/product-api/product-api-service/product-api-service.';
 import { ProductComponent } from './UI/view-models/product/product.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductComponent } from './UI/view-models/product/product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [{provide: ProductGateway, useClass:ProductApiService }],
   bootstrap: [AppComponent]
